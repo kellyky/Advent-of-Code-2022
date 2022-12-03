7c3c42c2e1fb4b2f25f16c4a8952af972de5df0f
Kelly Young <kelpopko@gmail.com>
2022-12-02 18:25:53 -0600

    Simplify

---
+++ 1_day_calorie_counting/1_carrying_most_calories.rb
@@ -0,0 +1,37 @@
+loads = File.read('puzzle_input.txt')
+
+puts loads.is_a? String
+
+line_of_elves = loads.split(/(\s)/)
+line_of_elves.reject!{ |el| el == "\n" }
+
+elves = {}
+groups = []
+
+i = 0
+while i < line_of_elves.count
+  this_index = i
+  last_index = i - 1
+
+  if (line_of_elves[last_index] == "" || line_of_elves[last_index] ==nil) && line_of_elves[this_index] != ""
+    groups.push(last_index)
+  end
+
+  i += 1
+end
+
+groups.each.with_index do |group, i|
+  beginning = i == 0 ? 0 : groups[i - 1] 
+  ending = groups[i]
+  
+  elves[i] = line_of_elves
+    .slice(beginning...ending).to_a
+    .map(&:to_i)
+    .sum
+end
+
+def get_max_cal_count(hash)
+  hash.values.max
+end
+
+puts get_max_cal_count(elves)
