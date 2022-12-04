3f9d75d7708c2ad24031f324a481a1bc5fb695b6
Kelly Young <kelpopko@gmail.com>
2022-12-04 11:29:20 -0600

    Add part 2 solution

---
+++ 4_day_camp_cleanup/2_assignment_pairs_no_overlap.rb
@@ -0,0 +1,28 @@
+assignment_pairs = File.read("input.txt").split
+
+class Cleanup
+  def initialize(assignment_pairs)
+    @assignment_pairs = assignment_pairs
+  end
+
+  def count_overlapping_assignments
+    find_overlapping_assignments.select{|el| el == true }.count
+  end
+
+  def find_overlapping_assignments
+    integer_quartets.map do |quartet|
+      a, b, c, d = quartet
+      (c..d).include?(a) || (c..d).include?(b) || (a..b).include?(c) || (a..b).include?(d)
+    end
+  end
+
+  def integer_quartets
+    @assignment_pairs.map do |pair|
+       pair.split(/,|-/).map(&:to_i)
+    end
+  end
+
+end
+
+assignment_pairs = Cleanup.new(assignment_pairs)
+puts assignment_pairs.count_overlapping_assignments
