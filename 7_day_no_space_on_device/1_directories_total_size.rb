db95f54fe75f5b232aca67b1e6f1f5e898054fd4
Kelly Young <kelpopko@gmail.com>
2022-12-27 10:28:08 -0600

    refactorrrr

--- 7_day_no_space_on_device/1_directories_total_size.rb
+++ 7_day_no_space_on_device/1_directories_total_size.rb
@@ -1,44 +1,82 @@
-require 'pry-byebug'
-lines_of_code = File.read('sample_input.txt').lines 
-# lines_of_code = File.read('input.txt').lines 
+# require 'pry-byebug'
+lines_of_code = File.readlines('sample_input.txt', chomp: true) 
+# lines_of_code = File.readlines('input.txt', chomp: true) 
 
 class DeviceMemory
+  @@total_sum = 0
+
   def initialize lines_of_code
     @lines_of_code = lines_of_code
+    @directory_history = ["/"]
+  end
+
+  def directories_max(hash)
+    # find sum of directories totally no more than 100000
   end
 
   def tree
-    tree = {}
-    directory_history = ["/"]
+    @tree = Hash.new 
+      cd = @directory_history.last
+      @tree[cd] = {}  
 
     ls_chonk.map.with_index do |listing, i|
-      cd = directory_history.last
-      tree[cd] = {}  # FIXME doesn't nest
+      file_sum = get_file_sum(listing)
+      directories = get_directories(listing)
 
-      files = listing.select{ |r| r.match(/[0-9]/)}
-      files = files.map do |f|
-        _, file_size = f.split.reverse
-        file_size.to_i
+      *path, last = listing
+      puts i
+      puts path.to_s
+      puts last.to_s
+
+      @cd_commands = change_directory(listing)
+      # file_sum = @files.map(&:to_i).sum
+
+      directories.each do |folder|
+        path.reduce(@tree, :fetch)[last][folder] = Hash.new
       end
 
-      tree[cd]["the_files"] = files.map(&:to_i).sum
+      path.reduce(@tree, :fetch)[last]["file_sum"] = file_sum # file_sum
 
-      listing.each do |ls|
+      @cd_commands.each do |command|
+        command == "." ? @directory_history.pop : @directory_history.push(command)
+      end
 
-        if ls.match((/\$ cd/))
-          dir_pointer = ls[-2]
-          if dir_pointer == "."
-            directory_history.pop
-            cd = directory_history.last
-            next
-          else
-            directory_history.push(dir_pointer)
-          end
-        end
+    end
+    @tree
+  end
 
+  def change_directory(arr)
+    folders = arr.select do |string|
+      string.match('cd')
+    end
+
+    folders.map { |folder| folder.split(" ").last }
+    folders.each do |folder|
+      if folder == ".." 
+        @directory_history.pop
+      else
+        @directory_history.push(folder)
       end
     end
-    tree
+  end
+
+  def get_directories(arr)
+    folders = arr.select do |string|
+      string.match('dir')
+    end
+
+    folders.map { |folder| folder.split(" ").last }
+  end
+
+  def get_file_sum(arr)
+    files = arr.select do |string|
+      string.match(/[0-9]/)
+    end
+
+    files = files.map do |f|
+      _, file_size = f.split.reverse
+      file_size.to_i
+    end.sum
   end
 
   # chonks of ls results (array of subarrays)
@@ -59,4 +97,6 @@ end
   
 
 drive = DeviceMemory.new(lines_of_code)
-puts drive.tree
+drive.tree
+puts drive.ls_chonk.to_s
+# puts drive.change_directory(["584 i", "$ cd ..", "$ cd ..", "$ cd d"])
--- 8_treetop_house/1_visible_trees.rb
+++ 8_treetop_house/1_visible_trees.rb
@@ -6,69 +6,101 @@ class Forest
     @forest = forest
   end
 
-
-  def print_it
-    @forest
+  def total
+    perimeter + interior  
   end
 
-  def count_boundary_trees
-    2 * (@forest.first.length + @forest.length)
+  def interior
+    # @coords = from_east + from_west + from_south + from_north
+    # @coords.uniq.count / 2
   end
 
-  def trees_visible_from_west
-    # left to right
-    @visible_trees = 0
-    forest_map.each do |k, row|
-      last_tree = 0
 
-      row.each_with_index.reduce(0) do |memo, (x, i)|
-        while x > last_tree
-          @visible_trees += 1
-          last_tree = x
+  def perimeter_coord(x, y)
+     x == (0 || trees_per_row || trees_per_row ) || y == (0 || trees_per_row || trees_per_row ) 
+  end
+
+
+  def from_west
+    @west = get_visible_interior_trees(@forest).uniq
+    @west
+  end
+
+  def from_east
+    coords = get_visible_interior_trees(@forest).uniq
+
+    coords.uniq.map do |coord|
+      x, y = coord
+      [trees_per_row - x, trees_per_row - y] 
+    end
+  end
+
+  def from_north
+    @north = get_visible_interior_trees(forest_columns)
+
+    @north
+    # @north.uniq.map do |coord|
+    #   x, y = coord
+    #   [x, y]
+    #   # [y, x]
+    # end
+  end
+
+
+  def from_south
+    @south = get_visible_interior_trees(forest_columns)
+
+    @south.uniq.map do |coord|
+      x, y = coord
+      [trees_per_row - x, trees_per_row - y] 
+    end
+  end
+
+  def get_visible_interior_trees(arr)
+    @interior_coords = []
+    arr.each.with_index do |row, j|
+      row.chars.each.with_index do |tree, i|
+        unless perimeter_coord(i, j)
+          this_tree = tree.to_i
+          last_tree = row[j - 1].to_i 
+          if this_tree > last_tree
+            @interior_coords.push([i, j])
+          end
         end
       end
+    end 
 
+    @interior_coords
+  end
+
+  def perimeter
+    4 * trees_per_row - 4
+  end
+
+  def trees_per_row
+    @forest.first.length
+  end
+
+  def forest_columns
+    @forest_columns = tree_columns_to_rows(@forest)
+  end
+
+  def tree_columns_to_rows(arr)
+    (0...arr.length).to_a.map do |i|
+      (0...arr[i].length).map { |j| arr[j][i] }.join
     end
-    @visible_trees
-  end
-
-  def trees_visible_from_east
-    # right to left
-  end
-
-  def trees_visible_from_north
-    # top to bottom
-  end
-
-  def trees_visible_from_south
-    # bottom to top
-  end
-
-  def forest_map_from_west
-    @tree_map = {}
-
-    @forest.each.with_index do |row, i|
-      @tree_map[i + 1] = row.chars.map(&:to_i)
-    end
-
-    @tree_map
-  end
-
-  def forest_map_from_east
-
   end
 
 end
 
 kettle_moraine = Forest.new(forest)
-puts kettle_moraine.trees_visible_from_west
+puts "perimeter trees: #{kettle_moraine.perimeter}"
 
-# def tree_map_reverse(forest_map)
-#   forest_map.map do |k, v|
-#     forest_map[k] = v.reverse
-#   end
-# end
+puts "visible trees from the west: #{kettle_moraine.from_west}"
+puts "visible trees from the east: #{kettle_moraine.from_east}"
+puts "east/west: #{kettle_moraine.from_west.concat(kettle_moraine.from_east).uniq}"
 
-# puts forest_map
-# puts ""
-# print tree_map_reverse(forest_map)
+puts "\nvisible trees from the north: #{kettle_moraine.from_north}"
+puts "visible trees from the south: #{kettle_moraine.from_south}"
+
+puts "north/south: #{kettle_moraine.from_north.concat(kettle_moraine.from_south).uniq.map{ |coord| coord.reverse }.concat(kettle_moraine.from_west).concat(kettle_moraine.from_east).uniq}"
